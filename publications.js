75d646c6dc1dc5e58d21c8d89da99d3d3c9d0402
Rezeck <rezeck@dcc.ufmg.br>
2020-11-07 23:03:34 -0300

    Update publications.js

--- publications.js
+++ publications.js
@@ -1,31 +1,31 @@
-window.onload = function () {
-console.log("[DEBUG] Loading page...");
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	theme: "light1",
-	title:{
-		text: ""
-	},
-	data: [{        
-		type: "column",
-      	indexLabelFontSize: 16,
-        name: "Number of Publications",
-        color: "#014D65",
-        showInLegend: "true",
-		dataPoints: [
-			{ y: 450, label:"1990"},
-			{ y: 414, label:"1991"},
-			{ y: 460, label:"1992"},
-			{ y: 450, label:"1993"},
-			{ y: 500, label:"1994"},
-			{ y: 480, label:"1995"},
-			{ y: 480, label:"1996"},
-			{ y: 500, label:"1997"},
-			{ y: 480, label:"1998"},
-			{ y: 510, label:"1999"}
-		]
-	}]
-});
-chart.render();
-console.log("[DEBUG] Loading page... done!");
-}
+var mydata = [
+				{ label: "1990",  y: 10  },
+				{ label: "1991", y: 15  },
+				{ label: "1992", y: 25  },
+				{ label: "1993",  y: 30  },
+				{ label: "1994",  y: 10  },
+				{ label: "1995", y: 15  },
+				{ label: "1996", y: 35  },
+				{ label: "1990",  y: 30  },
+				{ label: "1990",  y: 10  },
+				{ label: "1990", y: 15  },
+				{ label: "1990", y: 25  },
+				{ label: "1990",  y: 30  },
+				{ label: "1990",  y: 10  },
+				{ label: "1990", y: 15  },
+				{ label: "1990", y: 25  },
+				{ label: "1990",  y: 30  },
+				{ label: "1990",  y: 10  },
+				{ label: "1990", y: 15  },
+				{ label: "1990", y: 25  },
+				{ label: "1990",  y: 30  },
+				{ label: "1990",  y: 10  },
+				{ label: "1990", y: 15  },
+				{ label: "1990", y: 25  },
+				{ label: "1990",  y: 30  },
+				{ label: "1990",  y: 10  },
+				{ label: "1990", y: 15  },
+				{ label: "1990", y: 25  },
+				{ label: "1990",  y: 30  },
+				{ label: "1990",  y: 28  }
+			];
